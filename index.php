03c3966a4dc19b544622e5b022d845756d03d90b
Kristian Brouer Seedorff <seedorff.himself@gmail.com>
2012-12-06 18:54:37 +0100

    nice med nice

--- index.php
+++ index.php
@@ -58,110 +58,113 @@
 		</heading>
 
 		<div id="content" class="row">
-			<div class="eight columns">
-				<h1>Vejret nu og her</h1>
-				<div id="tabs">
-					<ul>
-						<li><a href="#tabs-1"><h2>Time for time</h2></a></li>
-						<li><a href="#tabs-2"><h2>3-9 døgns udsigt</h2></a></li>
-						<div style="clear:both;"></div>
-					</ul>
-					<div id="tabs-1">
-						<img src="http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by=<?php echo $maps->result->address_component[7]->long_name; ?>&mode=long">
+			<div class="border">
+				<div class="eight columns">
+					<h1>Vejret nu og her</h1>
+					<div id="tabs">
+						<ul>
+							<li><a href="#tabs-1"><h2>Time for time</h2></a></li>
+							<li><a href="#tabs-2"><h2>3-9 døgns udsigt</h2></a></li>
+							<div style="clear:both;"></div>
+						</ul>
+						<div id="tabs-1">
+							<img src="http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by=<?php echo $maps->result->address_component[7]->long_name; ?>&mode=long">
+						</div>
+						<div id="tabs-2">
+							<img src="http://servlet.dmi.dk/byvejr/servlet/byvejr?by=1000&tabel=dag3_9">
+						</div>
+						
 					</div>
-					<div id="tabs-2">
-						<img src="http://servlet.dmi.dk/byvejr/servlet/byvejr?by=1000&tabel=dag3_9">
+					<div id="udsigt">
+						<h1><?php echo $xml->channel->title; ?></h1>
+						<h2><?php echo $xml->channel->item->title; ?></h2>
+						<p><?php echo $xml->channel->item->description; ?></p>
 					</div>
+
 					
 				</div>
-				<div id="udsigt">
-					<h1><?php echo $xml->channel->title; ?></h1>
-					<h2><?php echo $xml->channel->item->title; ?></h2>
-					<p><?php echo $xml->channel->item->description; ?></p>
+				<div class="four columns">
+						
+					<h1>Radar</h1>
+					<img src="http://www.dmi.dk/dmi/radar-animation640.gif">
+
+					<h1>Aktuelle varsler</h1>
+					<img src="http://www.dmi.dk/dmi/danmark/regionaludsigten/varsel_oversigt.png" />
+					
+					<h1>Beskriv vejret nær dig!</h1>
+					<form action="post_comment.php" method="post">
+						<ul>
+							<li style="position: absolute; left: -9999999px;"><input type="text" name="comment_on" size=40 readonly="readonly" value="<?php print md5($_SERVER['PHP_SELF']); ?>" /></li>
+							<li><input type="text" size=40 name="comment_by" placeholder="Navn" /></li>
+							<li><textarea name="comment" cols=30 placeholder="Din beskrivelse"></textarea></li>
+							<li class="buttons"><input type="submit" value="Indsend" /></li>
+						</ul>
+					</form>
+					<?php
+						/* U need to change below fields */
+						$db_sitename="lokal_vejr";
+						$db_hostname="localhost";
+						$db_username="root";
+						$db_password="root";
+						$no_of_comments="6";
+						 
+						/* Leave the script below as it is */
+						mysql_connect($db_hostname, $db_username, $db_password);
+						mysql_select_db($db_sitename);
+						$pagename=md5($_SERVER['PHP_SELF']);
+						$query=mysql_query("Select * from comments where comment_on='$pagename' ORDER BY id DESC LIMIT 0, $no_of_comments");
+						
+						 
+						echo "<h3>Seneste vejrbeskrivelser</h3>";
+						while($fetch=mysql_fetch_array($query)) {
+						echo "<p>".$fetch['comment']."<br/><sub><b>Comment by: </b>".$fetch['comment_by']."</sub><hr /><p>";
+						}
+						mysql_close();
+					?>
+				
+				<script type="text/javascript">
+
+				      if (navigator.geolocation) {
+
+				        navigator.geolocation.getCurrentPosition(getInitialCoordinates);
+
+				        // watch for changes in position
+
+
+				      } else {
+
+				        alert("Error: Your browser doesn't support geolocation.");
+
+				      }
+
+				       
+
+				      function getInitialCoordinates(position) {
+
+				 			
+
+				 		var base = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
+						var latlng = position.coords.latitude + "," + position.coords.longitude;
+						var end = "&sensor=false";	 
+				 		
+				 		//alert(latlng);
+
+				 		//xmlDoc=loadXML(base + latlng + end);
+
+						//zip = xmlDoc.getElementsByTagName("results[0].address_component[7].long_name[0]");
+
+						//alert(base + latlng + end);
+				      }
+
+						
+					
+				</script>
+
+
+		
+				
 				</div>
-
-				
-			</div>
-			<div class="four columns">
-					
-				<h1>Radar</h1>
-				<img src="http://www.dmi.dk/dmi/radar-animation640.gif">
-
-				<h1>Aktuelle varsler</h1>
-				<img src="http://www.dmi.dk/dmi/danmark/regionaludsigten/varsel_oversigt.png" />
-				
-				<h1>Beskriv vejret nær dig!</h1>
-				<form action="post_comment.php" method="post">
-					<ul>
-						<li style="opacity: 0;"><input type="text" name="comment_on" size=40 readonly="readonly" value="<?php print md5($_SERVER['PHP_SELF']); ?>" /></li>
-						<li><input type="text" size=40 name="comment_by" placeholder="Navn" /></li>
-						<li><textarea name="comment" cols=30 placeholder="Din beskrivelse"></textarea></li>
-						<li class="buttons"><input type="submit" value="Indsend" /></li>
-					</ul>
-				</form>
-				<?php
-					/* U need to change below fields */
-					$db_sitename="lokal_vejr";
-					$db_hostname="localhost";
-					$db_username="root";
-					$db_password="root";
-					$no_of_comments="6";
-					 
-					/* Leave the script below as it is */
-					mysql_connect($db_hostname, $db_username, $db_password);
-					mysql_select_db($db_sitename);
-					$pagename=md5($_SERVER['PHP_SELF']);
-					$query=mysql_query("Select * from comments where comment_on='$pagename' ORDER BY id DESC LIMIT 0, $no_of_comments");
-					
-					 
-					echo "<h3>Seneste vejrbeskrivelser</h3>";
-					while($fetch=mysql_fetch_array($query)) {
-					echo "<p>".$fetch['comment']."<br/><sub><b>Comment by: </b>".$fetch['comment_by']."</sub><hr /><p>";
-					}
-					mysql_close();
-				?>
-			
-			<script type="text/javascript">
-
-			      if (navigator.geolocation) {
-
-			        navigator.geolocation.getCurrentPosition(getInitialCoordinates);
-
-			        // watch for changes in position
-
-
-			      } else {
-
-			        alert("Error: Your browser doesn't support geolocation.");
-
-			      }
-
-			       
-
-			      function getInitialCoordinates(position) {
-
-			 			
-
-			 		var base = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
-					var latlng = position.coords.latitude + "," + position.coords.longitude;
-					var end = "&sensor=false";	 
-			 		
-			 		//alert(latlng);
-
-			 		//xmlDoc=loadXML(base + latlng + end);
-
-					//zip = xmlDoc.getElementsByTagName("results[0].address_component[7].long_name[0]");
-
-					//alert(base + latlng + end);
-			      }
-
-					
-				
-			</script>
-
-
-	
-			
+		
 			</div>
 		</div>
 
